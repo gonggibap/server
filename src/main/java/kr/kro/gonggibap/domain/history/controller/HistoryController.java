022e135cf916bf2cdd83309168329bcc97a7b8be
minhoKK <koho1047@naver.com>
2024-10-22 10:37:39 +0900

    feat : #1 History 컨트롤러,서비스,레포지토리, 엔티티 추가

---
+++ src/main/java/kr/kro/gonggibap/domain/history/controller/HistoryController.java
@@ -0,0 +1,16 @@
+package kr.kro.gonggibap.domain.history.controller;
+
+import kr.kro.gonggibap.domain.history.service.HistoryService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/history")
+public class HistoryController {
+
+    private final HistoryService historyService;
+}
