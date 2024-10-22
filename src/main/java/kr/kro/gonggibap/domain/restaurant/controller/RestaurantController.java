4473ae7eee66832070f70c4d2d83469e41892473
minhoKK <koho1047@naver.com>
2024-10-22 15:09:18 +0900

    feat : #3 식당 조회 페이징 처리

--- src/main/java/kr/kro/gonggibap/domain/restaurant/controller/RestaurantController.java
+++ src/main/java/kr/kro/gonggibap/domain/restaurant/controller/RestaurantController.java
@@ -1,10 +1,19 @@
 package kr.kro.gonggibap.domain.restaurant.controller;
 
+import kr.kro.gonggibap.domain.restaurant.dto.response.RestaurantPageResponse;
 import kr.kro.gonggibap.domain.restaurant.service.RestaurantService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -13,4 +22,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class RestaurantController {
 
     private final RestaurantService restaurantService;
+
+    @GetMapping()
+    public ResponseEntity<?> getRestaurant(@RequestParam List<BigDecimal> latitudes,
+                                                        @RequestParam List<BigDecimal> longitudes,
+                                                        @PageableDefault(page = 0, size = 30) Pageable pageable){
+
+        RestaurantPageResponse response = restaurantService.getRestaurant(latitudes, longitudes, pageable);
+        return ResponseEntity.ok(response);
+    }
 }
--- src/main/java/kr/kro/gonggibap/domain/restaurant/service/RestaurantService.java
+++ src/main/java/kr/kro/gonggibap/domain/restaurant/service/RestaurantService.java
@@ -1,10 +1,17 @@
 package kr.kro.gonggibap.domain.restaurant.service;
 
+import kr.kro.gonggibap.domain.restaurant.dto.response.RestaurantPageResponse;
+import kr.kro.gonggibap.domain.restaurant.dto.response.RestaurantResponse;
 import kr.kro.gonggibap.domain.restaurant.repository.RestaurantRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 @Slf4j
 @Transactional(readOnly = true)
@@ -13,4 +20,21 @@ import org.springframework.transaction.annotation.Transactional;
 public class RestaurantService {
 
     private final RestaurantRepository restaurantRepository;
+
+    public RestaurantPageResponse getRestaurant(List<BigDecimal> latitudes, List<BigDecimal> longitudes, Pageable pageable) {
+
+        StringBuilder polygon = new StringBuilder("POLYGON((");
+        polygon.append(longitudes.get(0)).append(" ").append(latitudes.get(0)).append(", ")
+                .append(longitudes.get(1)).append(" ").append(latitudes.get(1)).append(", ")
+                .append(longitudes.get(2)).append(" ").append(latitudes.get(2)).append(", ")
+                .append(longitudes.get(3)).append(" ").append(latitudes.get(3)).append(", ")
+                .append(longitudes.get(0)).append(" ").append(latitudes.get(0)).append("))");
+
+        log.info("지도 조회 POLYGON 검색 범위 : {}", polygon);
+
+        Page<RestaurantResponse> restaurantResponses = restaurantRepository.getRestaurant(polygon.toString(), pageable);
+
+        return new RestaurantPageResponse(restaurantResponses.getTotalPages(),
+                restaurantResponses.getContent());
+    }
 }
