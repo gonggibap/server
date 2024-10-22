90d3a0e3ad4cecdb59f7cb24e203845d0500a31d
minhoKK <koho1047@naver.com>
2024-10-22 22:56:17 +0900

    bugfix : #19 조회시 좌표계 SRID 4326 적용

--- src/main/java/kr/kro/gonggibap/domain/restaurant/repository/RestaurantRepository.java
+++ src/main/java/kr/kro/gonggibap/domain/restaurant/repository/RestaurantRepository.java
@@ -12,7 +12,7 @@ public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
     @Query(value = "SELECT new kr.kro.gonggibap.domain.restaurant.dto.response.RestaurantResponse(r.id, r.restaurantName, r.link, r.category, r.address, r.roadAddress, r.latitude, r.longitude, h.publicOffice.id, COUNT(h)) " +
             "FROM Restaurant r " +
             "LEFT JOIN r.histories h " +
-            "WHERE FUNCTION('ST_Contains', FUNCTION('ST_GeomFromText', :polygon), r.location) = true " +
+            "WHERE FUNCTION('ST_Contains', FUNCTION('ST_GeomFromText', :polygon, 4326), r.location) = true " +
             "GROUP BY r.id " +
             "ORDER BY COUNT(h) desc")
     Page<RestaurantResponse> getRestaurant(String polygon, Pageable pageable);
