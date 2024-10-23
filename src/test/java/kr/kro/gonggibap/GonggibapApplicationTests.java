1977f2bdc512ecdac5a8b1f413b035030774331c
손지석 <thswltjr11@gmail.com>
2024-10-23 13:57:15 +0900

    bugfix: #28 테스트-contextLoad-오류-수정

--- src/test/java/kr/kro/gonggibap/GonggibapApplicationTests.java
+++ src/test/java/kr/kro/gonggibap/GonggibapApplicationTests.java
@@ -2,8 +2,10 @@ package kr.kro.gonggibap;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("local")
 public class GonggibapApplicationTests {
 
     @Test
