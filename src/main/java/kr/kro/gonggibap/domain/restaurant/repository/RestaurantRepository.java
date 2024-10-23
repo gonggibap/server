e5c542636ce754f1dea9eeb4e7f492f5ae191516
sunhong <psh56750855@gmail.com>
2024-10-23 21:00:16 +0900

    feat: #22 Repository 등록

--- src/main/java/kr/kro/gonggibap/domain/restaurant/repository/RestaurantRepository.java
+++ src/main/java/kr/kro/gonggibap/domain/restaurant/repository/RestaurantRepository.java
@@ -1,6 +1,7 @@
 package kr.kro.gonggibap.domain.restaurant.repository;
 
 import kr.kro.gonggibap.domain.restaurant.dto.response.RestaurantResponse;
+import kr.kro.gonggibap.domain.restaurant.dto.response.RestaurantSearchResponse;
 import kr.kro.gonggibap.domain.restaurant.entity.Restaurant;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -16,4 +17,14 @@ public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
             "GROUP BY r.id " +
             "ORDER BY COUNT(h) desc")
     Page<RestaurantResponse> getRestaurant(String polygon, Pageable pageable);
+
+    /**
+     * N-gram 기반 fulltext index를 restaurants 이름 기준으로 검색
+     */
+    @Query(value = "SELECT new kr.kro.gonggibap.domain.restaurant.dto.response.RestaurantSearchResponse(r.id, r.restaurantName, r.link, r.category, r.address, r.roadAddress, r.latitude, r.longitude ) " +
+            "FROM Restaurant r " +
+            "WHERE FUNCTION('match_against', r.restaurantName, :query) > 0 " +  // Check if the relevance score is positive
+            "ORDER BY FUNCTION('match_against', r.restaurantName, :query) DESC")
+    Page<RestaurantSearchResponse> searchRestaurant(String query, Pageable pageable);
+
 }
