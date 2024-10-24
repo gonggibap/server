c571dc622540b1374851bc418c914976973aada0
손지석 <thswltjr11@gmail.com>
2024-10-24 15:28:11 +0900

    refactor: #40 constant 패키지 분리 및 enum 추가

---
+++ src/main/java/kr/kro/gonggibap/core/constant/Adjective.java
@@ -0,0 +1,19 @@
+package kr.kro.gonggibap.core.constant;
+
+public enum Adjective {
+    정직한,
+    전무후무,
+    신속한,
+    화려한,
+    친절한,
+    고요한,
+    상냥한,
+    강력한,
+    평화로운,
+    든든한,
+    상쾌한,
+    새벽의,
+    한결같은,
+    유쾌한
+}
+
---
+++ src/main/java/kr/kro/gonggibap/core/constant/Food.java
@@ -0,0 +1,20 @@
+package kr.kro.gonggibap.core.constant;
+
+public enum Food {
+    치킨,
+    피자,
+    떡볶이,
+    라면,
+    김밥,
+    보쌈,
+    족발,
+    마라탕,
+    삼겹살,
+    햄버거,
+    회,
+    초밥,
+    냉면,
+    찜닭,
+    불고기
+}
+
