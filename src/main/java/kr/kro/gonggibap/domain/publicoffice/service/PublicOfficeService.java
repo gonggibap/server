a3038f820f80f1ab11607df2572779291a06d452
minhoKK <koho1047@naver.com>
2024-10-25 11:20:49 +0900

    feat : #55 공공기관 전체, 단일 조회 메소드

--- src/main/java/kr/kro/gonggibap/domain/publicoffice/service/PublicOfficeService.java
+++ src/main/java/kr/kro/gonggibap/domain/publicoffice/service/PublicOfficeService.java
@@ -1,10 +1,19 @@
 package kr.kro.gonggibap.domain.publicoffice.service;
 
+import kr.kro.gonggibap.core.exception.CustomException;
+import kr.kro.gonggibap.domain.publicoffice.dto.response.PublicOfficeResponse;
+import kr.kro.gonggibap.domain.publicoffice.entity.PublicOffice;
 import kr.kro.gonggibap.domain.publicoffice.repository.PublicOfficeRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static kr.kro.gonggibap.core.error.ErrorCode.NOT_FOUND_PUBLIC_OFFICE;
+import static kr.kro.gonggibap.domain.publicoffice.service.helper.PublicOfficeConverter.getPublicOfficeResponse;
+import static kr.kro.gonggibap.domain.publicoffice.service.helper.PublicOfficeConverter.getPublicOfficeResponses;
 
 @Slf4j
 @Transactional(readOnly = true)
@@ -13,4 +22,19 @@ import org.springframework.transaction.annotation.Transactional;
 public class PublicOfficeService {
 
     private final PublicOfficeRepository publicOfficeRepository;
+
+    public PublicOfficeResponse getPublicOffice(Long id) {
+        PublicOffice publicOffice = publicOfficeRepository.findById(id)
+                .orElseThrow(() -> new CustomException(NOT_FOUND_PUBLIC_OFFICE));
+
+        return getPublicOfficeResponse(publicOffice);
+    }
+
+
+    public List<PublicOfficeResponse> getPublicOffices() {
+        List<PublicOffice> publicOffices = publicOfficeRepository.findAll();
+
+        return getPublicOfficeResponses(publicOffices);
+    }
+
 }
