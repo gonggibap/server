3b88a8e584ca5c17387083164d2231776a45f253
손지석 <thswltjr11@gmail.com>
2024-10-31 10:19:27 +0900

    feat: #81 중복, 잘못된 접근 에러 처리

--- src/main/java/kr/kro/gonggibap/domain/restaurant/service/FavoriteRestaurantService.java
+++ src/main/java/kr/kro/gonggibap/domain/restaurant/service/FavoriteRestaurantService.java
@@ -1,17 +1,13 @@
 package kr.kro.gonggibap.domain.restaurant.service;
 
+import kr.kro.gonggibap.core.error.ErrorCode;
 import kr.kro.gonggibap.core.error.PageResponse;
-import kr.kro.gonggibap.domain.history.entity.History;
-import kr.kro.gonggibap.domain.history.service.HistoryService;
-import kr.kro.gonggibap.domain.publicoffice.dto.response.PublicOfficeResponse;
+import kr.kro.gonggibap.core.exception.CustomException;
 import kr.kro.gonggibap.domain.restaurant.dto.response.RestaurantResponse;
 import kr.kro.gonggibap.domain.restaurant.entity.FavoriteRestaurant;
 import kr.kro.gonggibap.domain.restaurant.entity.Restaurant;
 import kr.kro.gonggibap.domain.restaurant.repository.FavoriteRestaurantRepository;
-import kr.kro.gonggibap.domain.review.entity.Review;
-import kr.kro.gonggibap.domain.review.service.ReviewService;
 import kr.kro.gonggibap.domain.user.entity.User;
-import kr.kro.gonggibap.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
@@ -19,11 +15,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -36,16 +27,26 @@ public class FavoriteRestaurantService {
     @Transactional
     public void doFavoriteRestaurant(User user, Long restaurantId) {
         Restaurant findRestaurant = restaurantService.findRestaurantById(restaurantId);
+
+        // 중복된 식당일 시 conflict 예외 발생
+        if (favoriteRestaurantRepository.existsByUserAndRestaurant(user.getId(), findRestaurant.getId())) {
+            throw new CustomException(ErrorCode.DUPLICATED_FAVORITE_RESTAURANT);
+        }
+
         FavoriteRestaurant favoriteRestaurant = FavoriteRestaurant.builder()
                 .user(user)
                 .restaurant(findRestaurant)
                 .build();
+
         favoriteRestaurantRepository.save(favoriteRestaurant);
     }
 
     @Transactional
     public void unFavoriteRestaurant(User user, Long restaurantId) {
-        favoriteRestaurantRepository.deleteFavoriteRestaurant(user, restaurantId);
+        if (!favoriteRestaurantRepository.existsByUserAndRestaurant(user.getId(), restaurantId)) {
+            throw new CustomException(ErrorCode.NOT_FOUND_FAVORITE_RESTAURANT);
+        }
+        favoriteRestaurantRepository.deleteFavoriteRestaurant(user.getId(), restaurantId);
     }
 
     public PageResponse<?> getFavoriteList(final User user, final Pageable pageable) {
