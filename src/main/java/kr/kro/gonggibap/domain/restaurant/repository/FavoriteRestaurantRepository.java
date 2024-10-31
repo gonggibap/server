13cf1e0ddba97b30a99e5871e560fd7c2820eb49
손지석 <thswltjr11@gmail.com>
2024-10-31 09:26:03 +0900

    feat: #81 DTO Projection 방식으로 수정

--- src/main/java/kr/kro/gonggibap/domain/restaurant/repository/FavoriteRestaurantRepository.java
+++ src/main/java/kr/kro/gonggibap/domain/restaurant/repository/FavoriteRestaurantRepository.java
@@ -1,10 +1,10 @@
 package kr.kro.gonggibap.domain.restaurant.repository;
 
+import kr.kro.gonggibap.domain.restaurant.dto.response.RestaurantResponse;
 import kr.kro.gonggibap.domain.restaurant.entity.FavoriteRestaurant;
 import kr.kro.gonggibap.domain.user.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -22,8 +22,15 @@ public interface FavoriteRestaurantRepository extends JpaRepository<FavoriteRest
             "AND fr.restaurant.id = :restaurantId")
     void deleteFavoriteRestaurant(@Param("user") User user, @Param("restaurantId") Long restaurantId);
 
-    @EntityGraph(attributePaths = {
-            "restaurant"})
-    Page<FavoriteRestaurant> findByUser(User user, Pageable pageable);
+    @Query(value = "SELECT new kr.kro.gonggibap.domain.restaurant.dto.response.RestaurantResponse(r.id, r.restaurantName, r.phone, r.link, r.category, r.detailCategory, r.addressName, r.roadAddressName, r.latitude, r.longitude, h.publicOffice.id, p.name, COUNT(distinct h), AVG(rev.point)) " +
+            "FROM FavoriteRestaurant fr " +
+            "JOIN fr.restaurant r " +
+            "LEFT JOIN r.histories h " +
+            "LEFT JOIN r.reviews rev " +
+            "LEFT JOIN h.publicOffice p " +
+            "WHERE fr.user.id = :userId " +
+            "GROUP BY r.id " +
+            "ORDER BY COUNT(distinct h) desc")
+    Page<RestaurantResponse> findByUser(@Param("userId") Long userId, Pageable pageable);
 
 }
