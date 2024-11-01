ed26f0865137358685e77776a3ddc57149b70fbb
손지석 <thswltjr11@gmail.com>
2024-11-01 17:43:34 +0900

    feat: #103 ReviewResponse of 메소드 추가

--- src/main/java/kr/kro/gonggibap/domain/review/dto/response/ReviewResponse.java
+++ src/main/java/kr/kro/gonggibap/domain/review/dto/response/ReviewResponse.java
@@ -2,25 +2,46 @@ package kr.kro.gonggibap.domain.review.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import kr.kro.gonggibap.domain.image.entity.Image;
+import kr.kro.gonggibap.domain.review.entity.Review;
+import kr.kro.gonggibap.domain.user.entity.User;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
-@RequiredArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor
 public class ReviewResponse {
-    private final Long reviewId;
-    private final Long userId;
-    private final String userName;
-    private final Long userReviewCount;
-    private final Double userReviewAvg;
-    private final Double point;
-    private final String content;
+    private Long reviewId;
+    private Long userId;
+    private String userName;
+    private Long userReviewCount;
+    private Double userReviewAvg;
+    private Double point;
+    private String content;
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
-    private final LocalDateTime date;
-    private final List<String> imageUrls;
+    private LocalDateTime date;
+    private List<String> imageUrls;
+
+    public static ReviewResponse of(User user, Review review) {
+        ReviewResponse response = new ReviewResponse();
+        response.reviewId = review.getId();
+        response.userId = user.getId();
+        response.userName = user.getName();
+        response.userReviewCount = user.getReviewsCount();
+        response.userReviewAvg = user.getReviewsAverage();
+        response.point = review.getPoint();
+        response.content = review.getContent();
+        response.date = review.getLastModifiedDate();
+        response.imageUrls = review.getImages().stream()
+                .map(Image::getImageUrl).toList();
+        return response;
+    }
 }
