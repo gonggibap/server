ced6db7045bbba4882db9f2e53aa27c7c1e888b1
손지석 <thswltjr11@gmail.com>
2024-11-04 11:54:58 +0900

    refactor: #103 기존의 내 리뷰 조회를 페이징 처리로 대체 구현

--- src/main/java/kr/kro/gonggibap/domain/review/repository/ReviewRepository.java
+++ src/main/java/kr/kro/gonggibap/domain/review/repository/ReviewRepository.java
@@ -1,6 +1,8 @@
 package kr.kro.gonggibap.domain.review.repository;
 
 import kr.kro.gonggibap.domain.review.entity.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -24,6 +26,11 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
             "WHERE r.id = :reviewId")
     Optional<Review> findByIdWithImages(Long reviewId);
 
-    List<Review> findAllByUserId(Long userId);
+    @Query("SELECT r " +
+            "FROM Review r " +
+            "LEFT JOIN FETCH r.images " +
+            "WHERE r.user.id = :userId " +
+            "ORDER BY r.createdDate DESC")
+    List<Review> findPageByUserIdWithImages(@Param("userId") Long userId, Pageable pageable);
 
 }
