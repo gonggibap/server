c69c2f29c7209dad3f903c98faae34a9ac587531
손지석 <thswltjr11@gmail.com>
2024-11-04 11:52:35 +0900

    feat: #103 UserMyPageDto 추가

--- src/main/java/kr/kro/gonggibap/domain/user/dto/UserMyPageDto.java
+++ src/main/java/kr/kro/gonggibap/domain/user/dto/UserMyPageDto.java
@@ -4,22 +4,31 @@ import kr.kro.gonggibap.domain.restaurant.dto.response.RestaurantResponse;
 import kr.kro.gonggibap.domain.restaurant.entity.FavoriteRestaurant;
 import kr.kro.gonggibap.domain.review.dto.response.ReviewResponse;
 import kr.kro.gonggibap.domain.user.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserMyPageDto {
     private Long userId;
     private String userName;
     private String userEmail;
-    private List<ReviewResponse> userReviews;
     private List<RestaurantResponse> userFavoriteRestaurants;
+    private List<ReviewResponse> userReviews;
 
-    public static UserMyPageDto of(User user, List<ReviewResponse> reviewResponses, RestaurantResponse restaurantResponse) {
+    public static UserMyPageDto of(User user,
+                                   List<RestaurantResponse> favoriteRestaurants,
+                                   List<ReviewResponse> reviewResponse) {
         UserMyPageDto dto = new UserMyPageDto();
         dto.userId = user.getId();
         dto.userName = user.getName();
         dto.userEmail = user.getEmail();
-        dto.userReviews = reviewResponses;
-
+        dto.userFavoriteRestaurants = favoriteRestaurants;
+        dto.userReviews = reviewResponse;
+        return dto;
     }
 }
