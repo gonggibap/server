2bfcb3f587e32a0b7d0c055584a6e45f45a44802
nbo5548 <nbo5548@naver.com>
2024-11-06 14:31:08 +0900

    refactor: #114 SQL 컨벤션에 맞게 수정

--- src/main/java/kr/kro/gonggibap/domain/image/repository/ImageRepository.java
+++ src/main/java/kr/kro/gonggibap/domain/image/repository/ImageRepository.java
@@ -10,6 +10,7 @@ import java.util.List;
 public interface ImageRepository extends JpaRepository<Image, Long> {
 
 
-    @Query("SELECT i FROM Image i WHERE i.review.id = :reviewId")
+    @Query("SELECT i FROM Image i " +
+            "WHERE i.review.id = :reviewId")
     List<Image> findByReviewId(@Param("reviewId") Long reviewId);
 }
